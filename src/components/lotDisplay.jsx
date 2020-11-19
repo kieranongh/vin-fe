11dce0df7aafb7cf0ed3d95350da3fab96b7e582
Kieran Lomas <kieran.lomas@bom.gov.au>
2020-11-19 14:34:12 +0000

    Add edit button & avatar

--- src/components/lotDisplay.jsx
+++ src/components/lotDisplay.jsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+import Fab from '@material-ui/core/Fab';
+import Avatar from '@material-ui/core/Avatar';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import EditIcon from '@material-ui/icons/Edit';
 
 const useStyles = makeStyles((theme) => ({
   fullWidth: {
-    minWidth: 500
+    minWidth: 650
+  },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  secondaryLotText: {
+    padding: '0.5em'
+  },
+  rightTextAlign: {
+    textAlign: 'right'
+  },
+  editButtonContainer: {
+    position: 'relative'
+  },
+  editButton: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    backgroundColor: theme.palette.info.main,
+    color: theme.palette.common.white,
+    '&:hover': {
+      backgroundColor: theme.palette.info.dark,
+    }
+  },
+  avatar: {
+    backgroundColor: theme.palette.secondary.light
+  },
+  lotTitle: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1)
+    }
   }
+
 }))
 
 const LotDisplay = (props) => {
@@ -18,23 +54,45 @@ const LotDisplay = (props) => {
   const { lotCode, description, tankCode, volume, productState, ownerName, components } = props.selectedLot
   const { closeLotDisplay } = props
 
+  const [showEdit, setShowEdit] = useState(false)
+
+  const handleEditClick = () => {
+    setShowEdit(true)
+  }
+
+  const handleEditClose = () => {
+    setShowEdit(false)
+  }
+
   return (
     <div>
-      <Grid container alignItems="center" direction="column" spacing={3}>
-        <Grid item>
+      <Grid container alignItems="center" direction="column">
+        <Grid item className={`${classes.buttonContainer} ${classes.fullWidth}`}>
           <IconButton onClick={closeLotDisplay}>
             <ArrowBackIcon />
           </IconButton>
+          <div className={classes.editButtonContainer}>
+            <Fab className={classes.editButton} onClick={handleEditClick}>
+              <EditIcon />
+            </Fab>
+          </div>
         </Grid>
-        <Grid item>
-          {/* Avatar */}
-          <Typography variant="h4" className={classes.fullWidth}>
-            {lotCode}
-          </Typography>
-          <Typography variant="h6" className={classes.fullWidth}>
+        <Grid item className={classes.fullWidth}>
+          <div className={classes.lotTitle}>
+            <Avatar className={classes.avatar}>W</Avatar>
+            <Typography variant="h4">
+              {lotCode}
+            </Typography>
+          </div>
+          <Typography variant="h6">
             {description}
           </Typography>
-          <Grid container justify="space-between" spacing={1} className={classes.fullWidth}>
+          <Grid
+            container
+            justify="space-between"
+            spacing={1}
+            className={`${classes.secondaryLotText}`}
+          >
             <Grid item>
               <Typography>
                 Volume
@@ -49,7 +107,7 @@ const LotDisplay = (props) => {
                 Owner
               </Typography>
             </Grid>
-            <Grid item>
+            <Grid item className={classes.rightTextAlign}>
               <Typography>
                 {volume}
               </Typography>
