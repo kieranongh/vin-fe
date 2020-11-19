c5bbcdf1a97e927c802f52f7b712bc2f814d762b
Kieran Lomas <kieran.lomas@bom.gov.au>
2020-11-19 16:47:48 +0000

    Show popup when edit clicked

--- src/components/lotDisplay.jsx
+++ src/components/lotDisplay.jsx
@@ -5,6 +5,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Fab from '@material-ui/core/Fab';
+import Chip from '@material-ui/core/Chip';
 import Avatar from '@material-ui/core/Avatar';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import EditIcon from '@material-ui/icons/Edit';
@@ -30,13 +31,18 @@ const useStyles = makeStyles((theme) => ({
   },
   editButton: {
     position: 'absolute',
-    top: 0,
+    top: 30,
     right: 0,
     backgroundColor: theme.palette.info.main,
     color: theme.palette.common.white,
     '&:hover': {
       backgroundColor: theme.palette.info.dark,
     }
+  },
+  editPopup: {
+    position: 'fixed',
+    top: 20,
+    right: 20
   },
   avatar: {
     backgroundColor: theme.palette.secondary.light
@@ -67,67 +73,73 @@ const LotDisplay = (props) => {
   }
 
   return (
-    <div>
-      <Grid container alignItems="center" direction="column">
-        <Grid item className={`${classes.buttonContainer} ${classes.fullWidth}`}>
-          <IconButton onClick={closeLotDisplay}>
-            <ArrowBackIcon />
-          </IconButton>
-          <div className={classes.editButtonContainer}>
-            <Fab className={classes.editButton} onClick={handleEditClick}>
-              <EditIcon />
-            </Fab>
-          </div>
-        </Grid>
-        <Grid item className={classes.fullWidth}>
-          <div className={classes.lotTitle}>
-            <Avatar className={classes.avatar}>W</Avatar>
-            <Typography variant="h4">
-              {lotCode}
-            </Typography>
-          </div>
-          <Typography variant="h6">
-            {description}
-          </Typography>
-          <Grid
-            container
-            justify="space-between"
-            spacing={1}
-            className={`${classes.secondaryLotText}`}
-          >
-            <Grid item>
-              <Typography>
-                Volume
-              </Typography>
-              <Typography>
-                Tank code
-              </Typography>
-              <Typography>
-                Product state
-              </Typography>
-              <Typography>
-                Owner
-              </Typography>
-            </Grid>
-            <Grid item className={classes.rightTextAlign}>
-              <Typography>
-                {volume}
-              </Typography>
-              <Typography>
-                {tankCode}
-              </Typography>
-              <Typography>
-                {productState}
-              </Typography>
-              <Typography>
-                {ownerName}
-              </Typography>
-            </Grid>
-          </Grid>
-          <ComponentDisplay components={components} />
-        </Grid>
+    <Grid container alignItems="center" direction="column">
+      {showEdit && (
+        <Chip
+          label="Edit Clicked!!"
+          onDelete={handleEditClose}
+          color="primary"
+          className={classes.editPopup}
+          />
+      )}
+      <Grid item className={`${classes.buttonContainer} ${classes.fullWidth}`}>
+        <IconButton onClick={closeLotDisplay}>
+          <ArrowBackIcon />
+        </IconButton>
+        <div className={classes.editButtonContainer}>
+          <Fab className={classes.editButton} onClick={handleEditClick}>
+            <EditIcon />
+          </Fab>
+        </div>
       </Grid>
-    </div>
+      <Grid item className={classes.fullWidth}>
+        <div className={classes.lotTitle}>
+          <Avatar className={classes.avatar}>W</Avatar>
+          <Typography variant="h4">
+            {lotCode}
+          </Typography>
+        </div>
+        <Typography variant="h6">
+          {description}
+        </Typography>
+        <Grid
+          container
+          justify="space-between"
+          spacing={1}
+          className={`${classes.secondaryLotText}`}
+        >
+          <Grid item>
+            <Typography>
+              Volume
+            </Typography>
+            <Typography>
+              Tank code
+            </Typography>
+            <Typography>
+              Product state
+            </Typography>
+            <Typography>
+              Owner
+            </Typography>
+          </Grid>
+          <Grid item className={classes.rightTextAlign}>
+            <Typography>
+              {volume}
+            </Typography>
+            <Typography>
+              {tankCode}
+            </Typography>
+            <Typography>
+              {productState}
+            </Typography>
+            <Typography>
+              {ownerName}
+            </Typography>
+          </Grid>
+        </Grid>
+        <ComponentDisplay components={components} lotCode={lotCode} />
+      </Grid>
+    </Grid>
   )
 }
 
